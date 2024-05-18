3d3f50e9b8d222538d17f82559d2d053a4919453
Shukri Hussein <husseinshukri@outlook.com>
2024-05-18 12:12:18 -0400

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [array, setArray] = useState([]);
-
-
-  const fetchAPI = async () => {
-    const response = await axios.get("http://127.0.0.1:8080/api/users")
-    console.log(response.data.users);
-    setArray(response.data.users)
-  };
-
+  const [runs, setRuns] = useState([]);
+  const [distance, setDistance] = useState('');
+  const [time, setTime] = useState('');
 
   useEffect(() => {
-    fetchAPI()
-  },[])
+    axios.get('http://127.0.0.1:5000/api/runs')
+      .then(response => setRuns(response.data))
+      .catch(error => console.error('Error fetching runs:', error));
+  }, []);
 
+  const addRun = () => {
+    axios.post('http://127.0.0.1:5000/api/runs', { distance, time })
+      .then(response => setRuns([...runs, response.data]))
+      .catch(error => console.error('Error adding run:', error));
+  };
 
   return (
-    
     <div>
-      <h1>My React App</h1>
-      
-          {array.map((user, index) => (
-            <div key={index}>
-            <span>{user}</span>
-            <br></br>
-            </div>
-          ))
-        }
+      <h1>Run Tracker</h1>
+      <div>
+        <input
+          type="number"
+          placeholder="Distance (km)"
+          value={distance}
+          onChange={e => setDistance(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Time (minutes)"
+          value={time}
+          onChange={e => setTime(e.target.value)}
+        />
+        <button onClick={addRun}>Add Run</button>
+      </div>
+      <ul>
+        {runs.map(run => (
+          <li key={run.id}>
+            {run.distance} km in {run.time} minutes on {new Date(run.date).toLocaleDateString()}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
